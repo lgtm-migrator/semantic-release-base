e04abf4e96735e68897f93124b92cb8deea6543a
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-11-11 05:55:55 -0800

    Chore: Update Code of Conduct and Contributing files 🔮✨ (#57)

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  // Provides nice test output of what's being run
+  verbose: true,
+
+  // OS notifications of test results is an opt in feature, enable by setting
+  // a truthy env value in your shell environment
+  notify: Boolean(process.env.ENABLE_JEST_NOTIFICATIONS),
+
+  // Collect test coverage of source files (excluding stories), report
+  // text-summary for devs and lcov for reporting to Code Climate in CI/CD envs.
+  collectCoverage: true,
+  coverageReporters: ['text-summary', 'lcov'],
+  collectCoverageFrom: ['lib/**/*.js'],
+}
